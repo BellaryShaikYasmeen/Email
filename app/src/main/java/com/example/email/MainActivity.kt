808ddd298cda28d0f766ae08903b2b16b225c7a3
Yasmeen Shaik <bellaryshaikyasmeen@gmail.com>
2023-08-09 17:35:16 +0530

    sms related Code

--- app/src/main/java/com/example/email/MainActivity.kt
+++ app/src/main/java/com/example/email/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.email
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
+import android.widget.Toast
 import com.example.email.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,13 +20,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun emailSend() {
-        val i =Intent(Intent.ACTION_SEND)
+
+    /*    val i =Intent(Intent.ACTION_SEND)
         i.putExtra(Intent.EXTRA_EMAIL,binding.txtTo.text.toString())
         i.putExtra(Intent.EXTRA_SUBJECT,binding.txtSub.text.toString())
         i.putExtra(Intent.EXTRA_TEXT,binding.txtMsg.text)
         i.setType("message/rfc822")
-        startActivity(Intent.createChooser(i,"Choose Mail App"))
+        startActivity(Intent.createChooser(i,"Choose Mail App")*/
+        try {
+            var smsManager=SmsManager.getDefault()
+            smsManager.sendTextMessage(binding.mblTxt.text.toString(),null,binding.msgTxt.text.toString(),null,null)
+            Toast.makeText(this,"SMS Sent Succesfully",Toast.LENGTH_LONG).show()
+
+        }
+        catch ( e:java.lang.Exception)
+        {
+            Toast.makeText(this,"Sms Failed to send ,Please try again later",Toast.LENGTH_LONG).show()
+        }
+    }
 
 
     }
-}
+
